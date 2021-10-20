bf879c7dffebd463f894c9f5164aaca6b3cc948d
root <root@instance-1.us-central1-c.c.centered-osprey-329406.internal>
2021-10-20 04:48:23 +0000

    Port scanner

---
+++ main.py
@@ -0,0 +1,47 @@
+import socket, sys, threading
+from queue import Queue
+
+def scan_thread(ip, port):
+    while True:
+        port = q.get()
+        tcp_scan(ip, port)
+        q.task_done()
+
+def tcp_scan(ip,p):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        result = socket.connect_ex((ip,p))
+        if result == 0:
+            print(f"port {p}")
+        sock.close()
+    except socket.error:
+        print("Machine could not be reached")
+        sys.exit()
+    except KeyboardInterrupt:
+        print('Exiting...')
+        sys.exit()
+
+def base(addr):
+    addr_valid = True
+    try:
+        socket.inet_aton(addr)
+    except socket.error:
+        try:
+            addr = socket.gethostbyname(addr)
+        except socket.gaierror:
+            addr_valid = False
+            print("Hostname not resolved")
+
+    port_range = 80
+
+    if addr_valid:
+        for _ in range(port_range):
+            t = threading.Thread(target=scan_thread, args=[addr])
+            t.daemon = True
+            t.start()
+
+        for p in range(port_range):
+            q.put(p)
+
+        q.join()
